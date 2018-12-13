0f8bebda0965624495bfe3ea9f3e484a3b004a78
Antariksha1 <35954444+Antariksha1@users.noreply.github.com>
2018-12-13 14:03:01 +0530

    Update DemoServlet.java

--- src/main/java/myapp/DemoServlet.java
+++ src/main/java/myapp/DemoServlet.java
@@ -26,6 +26,6 @@ public class DemoServlet extends HttpServlet {
   public void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws IOException {
     resp.setContentType("text/plain");
-    resp.getWriter().println("{ \"name\": \"World\" }");
+    resp.getWriter().println("{ \"name\": \"Everyone, I am Antariksh and I have deployed a sample java code in Google Cloud by using Github repository\" }");
   }
 }
